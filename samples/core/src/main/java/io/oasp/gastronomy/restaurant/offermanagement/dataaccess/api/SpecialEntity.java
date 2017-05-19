924bf3afe972a67149572611eefef4f092d958b6
Höhne <andreas-philipp.hoehne@capgemini.com>
2017-05-19 11:45:30 +0200

    Excercise 2 OASP-TP erledigt

--- samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
+++ samples/core/src/main/java/io/oasp/gastronomy/restaurant/offermanagement/dataaccess/api/SpecialEntity.java
@@ -1,13 +1,16 @@
 package io.oasp.gastronomy.restaurant.offermanagement.dataaccess.api;
 
-import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
-import io.oasp.gastronomy.restaurant.general.dataaccess.api.ApplicationPersistenceEntity;
-import io.oasp.gastronomy.restaurant.offermanagement.common.api.Offer;
-
 import javax.persistence.Column;
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import io.oasp.gastronomy.restaurant.general.common.api.datatype.Money;
+import io.oasp.gastronomy.restaurant.general.dataaccess.api.ApplicationPersistenceEntity;
+import io.oasp.gastronomy.restaurant.offermanagement.common.api.Offer;
+import io.oasp.gastronomy.restaurant.offermanagement.common.api.Special;
+import io.oasp.gastronomy.restaurant.offermanagement.common.api.WeeklyPeriod;
 
 /**
  * The {@link ApplicationPersistenceEntity persistent entity} for a special.
@@ -16,7 +19,7 @@ import javax.persistence.Table;
  */
 @Entity(name = "Special")
 @Table(name = "Special")
-public class SpecialEntity {
+public class SpecialEntity extends ApplicationPersistenceEntity implements Special {
 
   private String name;
 
@@ -26,6 +29,8 @@ public class SpecialEntity {
   private WeeklyPeriodEmbeddable activePeriod;
 
   private Money specialPrice;
+
+  private static final long serialVersionUID = 1L;
 
   /**
    * Returns the name of this special.
@@ -83,9 +88,9 @@ public class SpecialEntity {
    *
    * @param activePeriod the {@link WeeklyPeriodEmbeddable active period} this special applies for.
    */
-  public void setActivePeriod(WeeklyPeriodEmbeddable activePeriod) {
+  public void setActivePeriod(WeeklyPeriod activePeriod) {
 
-    this.activePeriod = activePeriod;
+    this.activePeriod = (WeeklyPeriodEmbeddable) activePeriod;
   }
 
   /**
@@ -106,6 +111,28 @@ public class SpecialEntity {
   public void setSpecialPrice(Money specialPrice) {
 
     this.specialPrice = specialPrice;
+  }
+
+  @Override
+  @Transient
+  public Long getOfferId() {
+
+    if (this.offer == null) {
+      return null;
+    }
+    return this.offer.getId();
+  }
+
+  @Override
+  public void setOfferId(Long offerId) {
+
+    if (offerId == null) {
+      this.offer = null;
+    } else {
+      OfferEntity offerEntity = new OfferEntity();
+      offerEntity.setId(offerId);
+      this.offer = offerEntity;
+    }
   }
 
 }
